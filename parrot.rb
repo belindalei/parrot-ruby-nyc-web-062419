98d0d833f4e23ee881980df89e267abd5f09c91c
Belinda Lei <belinda.lei95@gmail.com>
2019-05-31 01:56:21 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(parrot="Squawk!")
-  return parrot
-  parrot
+def parrot(phrase="Squawk!")
+  puts phrase
+  return
 end 
   
